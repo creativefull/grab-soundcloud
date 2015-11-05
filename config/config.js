9e1bddfcdbc0fe425bb97b5399747c6b3e846afe
Shodiqul Muzaki <user.creativefull@gmail.com>
2015-11-05 23:56:02 +0700

    Adding First Fitur

---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.exports = function config() {
+	this.api_key = '212fd0f09196654e0661ff4570f7ab26';
+}
---
+++ index.js
@@ -0,0 +1,67 @@
+var request = require('request');
+var config = require('./config/config'), conf = new config();
+
+module.exports = {
+	getTrackDetail : function(url, cb) {
+		var gabungan = 'https://api.soundcloud.com/resolve.json?url=' + url + '&client_id=' + conf.api_key;
+		request({url : gabungan, json : true}, function (err, response, body) {
+			if (!body.errors) {
+				if (body.kind == 'track') {
+					return cb(null, body);
+				}
+				else {
+					return cb('Url Not Valid Song', null);
+				}
+			}
+			else {
+				return cb(body.errors, null);
+			}
+		});
+	},
+	getPlaylistDetail : function(url, cb) {
+		var gabungan = 'https://api.soundcloud.com/resolve.json?url=' + url + '&client_id=' + conf.api_key;
+		request({url : gabungan, json : true}, function (err, response, body) {
+			if (!body.errors) {
+				if (body.kind == 'playlist') {
+					return cb(null, body);
+				}
+				else {
+					return cb('Url Not Valid Playlist', null);
+				}
+			}
+			else {
+				return cb(body.errors, null);
+			}
+		});
+	},
+	getTracksPlaylist : function(url, cb) {
+		var trackList = [];
+		this.getPlaylistDetail(url, function(err, doc) {
+			if (doc.tracks.length != 0) {
+				doc.tracks.forEach(function(track) {
+					trackList.push(track)
+				})				
+				return cb(null, trackList)
+			}
+			else {
+				return cb({errors : 'Tracks Not Found'}, null)
+			}
+		});
+	},
+	getAuthorDetail : function(url, cb) {
+		var gabungan = 'https://api.soundcloud.com/resolve.json?url=' + url + '&client_id=' + conf.api_key;
+		request({url : gabungan, json : true}, function (err, response, body) {
+			if (!body.errors) {
+				if (body.kind == 'user') {
+					return cb(null, body);
+				}
+				else {
+					return cb('Url Not Valid User', null);
+				}
+			}
+			else {
+				return cb(body.errors, null);
+			}
+		});
+	}
+}
