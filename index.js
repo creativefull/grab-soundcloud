6d97519317352596d89884b93b0fd9a58223567f
Shodiqul Muzaki <user.creativefull@gmail.com>
2016-02-07 01:22:54 +0700

    rebuild function

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
 var request = require('request');
 var config = require('./config/config'), conf = new config();
 
+function grabUrl(url, cb) {
+	var gabungan = 'https://api.soundcloud.com/resolve.json?url=' + url + '&client_id=' + conf.api_key;
+	request({url : gabungan, json : true}, function (err, response, body) {
+		if (err) {
+			return cb('Error Get Url', null);
+		}
+		if (!body.errors) {
+			return cb(null, body);
+		}
+		else {
+			return cb(body.errors, null);
+		}
+	});	
+}
+
 module.exports = {
 	getTrackDetail : function(url, cb) {
-		var gabungan = 'https://api.soundcloud.com/resolve.json?url=' + url + '&client_id=' + conf.api_key;
-		request({url : gabungan, json : true}, function (err, response, body) {
-			if (err) {
-				return cb('Error Get Url', null);
-			}
-			if (!body.errors) {
-				if (body.kind == 'track') {
-					return cb(null, body);
+		grabUrl(url, function(err, output) {
+			if (!err) {
+				if (output.kind == 'track') {
+					return cb(null, output);
 				}
 				else {
-					return cb('Url Not Valid Song', null);
+					return cb(err, null);
 				}
 			}
 			else {
-				return cb(body.errors, null);
+				return cb(output.errors, null);
 			}
 		});
 	},
 	getPlaylistDetail : function(url, cb) {
-		var gabungan = 'https://api.soundcloud.com/resolve.json?url=' + url + '&client_id=' + conf.api_key;
-		request({url : gabungan, json : true}, function (err, response, body) {
+		grabUrl(url, function(err, body) {
 			if (err) {
-				return cb('Error Get Url', null);
+				return cb(err, null);
 			}
-			if (!body.errors) {
+			else {
 				if (body.kind == 'playlist') {
 					return cb(null, body);
 				}
 				else {
-					return cb('Url Not Valid Playlist', null);
+					return cb(err, null);
 				}
-			}
-			else {
-				return cb(body.errors, null);
 			}
 		});
 	},
@@ -56,11 +63,8 @@ module.exports = {
 	},
 	getAuthorDetail : function(url, cb) {
 		var gabungan = 'https://api.soundcloud.com/resolve.json?url=' + url + '&client_id=' + conf.api_key;
-		request({url : gabungan, json : true}, function (err, response, body) {
-			if (err) {
-				return cb('Error Get Url', null);
-			}
-			if (!body.errors) {
+		grabUrl(url, function (errors, body) {
+			if (!errors) {
 				if (body.kind == 'user') {
 					return cb(null, body);
 				}
@@ -69,7 +73,26 @@ module.exports = {
 				}
 			}
 			else {
-				return cb(body.errors, null);
+				return cb(errors, null);
+			}
+		});
+	},
+	urlDownload : function(url, cb) {
+		grabUrl(url, function(err, output) {
+			if (output != null) {
+				if (output.kind == 'track') {
+					var data = [];
+					request({ url : 'https://api.soundcloud.com/i1/tracks/' + output.id + '/streams?client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea&app_version=540273e'}, function(err, response, body) {
+						var data2 = JSON.parse(body);
+						return cb(null, data2.http_mp3_128_url);
+					})
+				}
+				else {
+					return cb('Url Not Valid Song', null);
+				}				
+			}
+			else {
+				return cb(err, null);
 			}
 		});
 	}
